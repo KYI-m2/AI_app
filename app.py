aba0764017351e2bde6638c5993b8978d44f9a7d
KYI-m2 <webapplication.m2@gmail.com>
2024-11-10 17:12:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import io 
 import keras
 from keras.models import model_from_json
