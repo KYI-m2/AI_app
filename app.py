d066e4d9030f557a100e135e495544c18218a54e
KYI-m2 <webapplication.m2@gmail.com>
2024-11-19 20:33:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -469,11 +469,12 @@ if choose == 'Image Classification' :
  
     st.title('การจำเเนกเเมลงด้วยภาพ/Image Classifycation')
     predicted_name = ''
-   
+    df_1 = pd.read_excel("output.xlsx",index_col = 0)
+    new_index = len(df_1)
     
 
     st.divider()
-
+    
 
     css = '''
     <link href="https://fonts.googleapis.com/css2?family=Niramit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Outfit:wght@100..900&display=swap" rel="stylesheet">
